b5e4b3ef1c438cd87999fab578b0ed2d6c2e2959
Strange-Philip <philipabakahmensah@gmail.com.com>
2024-01-01 22:16:25 +0000

    Initial Commit

---
+++ Swift Carousel App/Swift_Carousel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Carousel_AppApp.swift
+//  Swift Carousel App
+//
+//  Created by Philip Abakah on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Carousel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
